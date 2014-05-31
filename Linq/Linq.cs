ee22bbe74db687eecf87eba81385ea2942e1f652
andreipopescu <andrei_popescu87@yahoo.com>
2014-05-31 21:15:28 +0300

    add missing files

---
+++ Linq/Linq.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    public static class Linq
+    {
+        public static bool All<T> (this IEnumerable<T> list, Func<T, bool> func)
+        {
+            foreach (var item in list)
+            {
+                if(!func(item))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool Any<T> (this IEnumerable<T> list, Func<T, bool> func)
+        {
+            foreach (var item in list)
+            {
+                if(func(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static IEnumerable<T> Where<T> (this IEnumerable<T> list, Func<T, bool> func)
+        {
+            List<T> data = new List<T>();
+
+            foreach (var item in list)
+            {
+                if(func(item))
+                {
+                    data.Add(item);
+                }
+            }
+
+            return data;
+        }
+
+        public static IEnumerable<R> Select<T, R> (this IEnumerable<T> list, Func<T, R> func)
+        {
+            List<R> result = new List<R>();
+
+            foreach (var item in list)
+            {
+                result.Add(func(item));
+            }
+
+            return result;
+        }
+
+        public static Dictionary<R, List<T>> GroupBy<T, R>(this IEnumerable<T> list, Func<T, R> func)
+        {
+            Dictionary<R, List<T>> result = new Dictionary<R, List<T>>();
+
+            foreach (var item in list)
+            {
+                var key = func(item);
+
+                if (result.ContainsKey(key))
+                {
+                    result[key].Add(item);
+                }
+                else
+                {
+                    result.Add(key, new List<T>{ item });
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Linq/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestLinq2 test = new TestLinq2();
+            test.TestAllMethod();
+            test.TestAnyMethod();
+            test.TestWhereMethod();
+            test.TestSelectMethod();
+            test.TestGroupByMethod();
+        }
+    }
+}
