71d6dfea29b801557e4642462453e1f75db01dee
andreipopescu <andrei_popescu87@yahoo.com>
2014-05-31 23:22:13 +0300

    added Singles again stupid github app

---
+++ Linq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestLinq test = new TestLinq();
+            test.TestAllMethod();
+            test.TestAnyMethod();
+            test.TestWhereMethod();
+            test.TestSelectMethod();
+            test.TestGroupByMethod();
+            test.TestSingleOrDefaultMethod();
+            test.TestSingletMethod();
+        }
+    }
+}
